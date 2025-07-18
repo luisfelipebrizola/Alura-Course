592b05e9d254eb3dbdac2c2be0bb24614f752104
luisfelipebrizola <luisfelipebrizolaaquistapace@gmail.com>
2025-07-18 12:22:20 -0300

    primeiro projeto de verificaçãlo de idade em JS

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+let nome = prompt('Qual seu nome?');
+let idade = prompt('Informe sua idade:');
+if (idade >= 18) {
+    alert(nome + ' pode tirar a habilitação!');
+} else {
+    alert(nome + ' ainda não pode tirar a habilitação!');
+}
