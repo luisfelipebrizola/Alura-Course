fe675e568525abc01306df5d0fddea8ef18f678d
luisfelipebrizola <luisfelipebrizolaaquistapace@gmail.com>
2025-07-25 16:39:16 -0300

    comparações
    while, if e else

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,28 @@
-let nome = prompt('Qual seu nome?');
-let idade = prompt('Informe sua idade:');
-if (idade >= 18) {
-    alert(nome + ' pode tirar a habilitação!');
-} else {
-    alert(nome + ' ainda não pode tirar a habilitação!');
+alert('Boas Vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute; 
+let tent = 1; 
+
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10:');
+    //se chute for igual ao número secreto
+    if(chute == numeroSecreto){
+        alert(`Isso aí! Você descobriu o número Secreto (${numeroSecreto})`);
+        if(tent == 1){
+            alert('Você tentou uma vez!');
+        } else {
+            alert(`Você tentou ${tent} vezes!`)
+        }
+        
+    }else{
+        alert('Você errou!');
+        tent++
+        if(chute > numeroSecreto){
+        alert(`O número secreto é menor que o ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    
+    }
 }
