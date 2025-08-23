8ca72dddd5fba21aa823eeca75badd4a4c0e3d2a
luisfelipebrizola <luisfelipebrizolaaquistapace@gmail.com>
2025-08-23 16:26:34 -0300

    terminando jogo do numero secreto

--- Curso 2-JS/app.js
+++ Curso 2-JS/app.js
@@ -1,18 +1,51 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo Do Número Screto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
-exibirTextoNaTela('h1', 'Jogo Do Número Screto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirMensagemInicial();
+    
 
 function verificarChute() {
+    let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa'
     let chute = document.querySelector('input').value;
     console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1','Acertou!');
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}`;
+        exibirTextoNaTela('p', mensagemTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+       if(chute > numeroSecreto){
+        exibirTextoNaTela('p','O número secreto é menor');
+       } else{
+        exibirTextoNaTela('p','O núemro secreto é maior');
+       }
+       tentativas++;
+       limparCampo();
+    }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random()* 10 + 1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
