478cf05fc79deb2524d9a99c30fa22a2eee23c71
luisfelipebrizola <luisfelipebrizolaaquistapace@gmail.com>
2025-10-08 14:19:14 -0300

    Inicio Curso 3

---
+++ Curso 3/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,6 @@
+function sortear(){
+    let quant = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+    
+}
