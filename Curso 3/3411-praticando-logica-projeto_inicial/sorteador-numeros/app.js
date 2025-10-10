732504f53591e634170e254379aeb0d1216dbf4f
luisfelipebrizola <luisfelipebrizolaaquistapace@gmail.com>
2025-10-10 16:31:43 -0300

    update app.js

--- Curso 3/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ Curso 3/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,6 +1,20 @@
 function sortear(){
-    let quant = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quant = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
     
+
+    let sorteados = [];
+    let numero;
+
+    for (let i  = 0; i < quant; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
+}
+
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
