6aeeb922da02b851a3b5410f2602faa55a97a6be
luisfelipebrizola <luisfelipebrizolaaquistapace@gmail.com>
2025-10-15 13:35:22 -0300

    e-ticket

--- Curso 3/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
+++ Curso 3/3411-praticando-logica-projeto_inicial/carrinho-compras/js/app.js
@@ -1,10 +1,15 @@
+let totalGeral = 0;
+
+
 function limpar(){
     let total = document.getElementById('valor-total');
     let carrinho = document.getElementById('lista-produtos');
-    subtinicial = 0;
+    totalGeral = 0;
     total.innerHTML = 'R$0,00';
     carrinho.innerHTML = '';
 }
+
+limpar();
 
     
 function adicionar(){
@@ -14,16 +19,17 @@ function adicionar(){
     let quant = document.getElementById('quantidade').value;
     let total = document.getElementById('valor-total');
     let valorUni = valor * quant;
-    let subT = valor * quant;
     let carrinho = document.getElementById('lista-produtos');
+    totalGeral = totalGeral + valorUni;
     
-    
-    total.innerHTML = `R$${subT},00`;
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quant}x</span> ${nomeProduto} <span class="texto-azul">R$${valorUni},00</span>
     </section>`
+    total.innerHTML = `R$${totalGeral},00`;
+    document.getElementById('quantidade').value = '';
     
 }
-    
+
+
     
     
---
+++ Curso 3/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -0,0 +1,10 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso').value;
+    let quant = document.getElementById('qtd').value;
+    let quantPista = document.getElementById('qtd-pista').value
+    let quantSuperior = document.getElementById('qtd-superior').value;
+    let quantInferior = document.getElementById('qtd-inferior').value;
+    
+
+   
+}
